fa2d2b5593bff91f5279c414bcff9927d72ad92b
greenboy59 <81871149+greenboy59@users.noreply.github.com>
2022-04-20 21:05:03 +0200

    Incrémentation on product with the same id/color in cart array. product.js page is finished.

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,45 +1,41 @@
-"use strict";
+'use strict'
 
 // Déclaration des variables
 const cartItemsElement = document.querySelector('#cart__items')
 
-// Lecture des clés et objets stockées dans le local storage puis envoi vers objet 'cart'
-  let produitsEnregistresDansLocalStorage = localStorage.getItem('panier');
-  let cart = JSON.parse(produitsEnregistresDansLocalStorage);
+const cart = JSON.parse(localStorage.getItem('cart'))
 
 // Injection dans le DOM
 function displayCart(products) {
   cart.forEach((item) => {
-    products.forEach((product) => {
-      cartItemsElement.innerHTML +=
-        `<article class="cart__item" data-id="${item.id_produit}" data-color="${item.couleur}">
+    const product = products.find(product => item.id === product._id)
+    cartItemsElement.innerHTML +=
+      `<article class="cart__item" data-id="${item.id}" data-color="${item.color}">
           <div class="cart__item__img">
-                  <img src="${product.imageUrl}" alt="Photographie d'un canapé">
-                </div>
-                <div class="cart__item__content">
-                  <div class="cart__item__content__description">
-                    <h2>${product.name}</h2>
-                    <p>${product.color}</p>
-                    <p>${product.price} €</p>
-                  </div>
-                  <div class="cart__item__content__settings">
-                    <div class="cart__item__content__settings__quantity">
-                      <p>Qté : ${item.quantité}</p>
-                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="0">
-                    </div>
-                    <div class="cart__item__content__settings__delete">
-                      <p class="deleteItem">Supprimer</p>
-                    </div>
-                </div>
+            <img src="${product.imageUrl}" alt="Photographie d'un canapé">
+          </div>
+          <div class="cart__item__content">
+            <div class="cart__item__content__description">
+              <h2>${product.name}</h2>
+              <p>${item.color}</p>
+              <p>${product.price} €</p>
+          </div>
+          <div class="cart__item__content__settings">
+            <div class="cart__item__content__settings__quantity">
+              <label>Qté:</label>
+              <p> </p>
+              <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${item.quantity}">
+            </div>
+            <div class="cart__item__content__settings__delete">
+              <p class="deleteItem">Supprimer</p>
+            </div>
+          </div>
         </div>
-    </article>`
-    })
+      </article>`
   })
 }
 
 // Récupération des données de l'API
-
-
 fetch(`http://localhost:3000/api/products`)
   .then(response => response.json())
   .then(products => displayCart(products))
--- front/js/product.js
+++ front/js/product.js
@@ -1,85 +1,99 @@
-'use strict';
-
+"use strict";
 // Création des variables
 
 // Variables de ciblage du DOM
-const image = document.querySelector('.item__img')
-const title = document.querySelector('#title')
-const price = document.querySelector('#price')
-const description = document.querySelector('#description')
-const colorSelectElement = document.querySelector('#colors')
-const quantitySelectElement = document.querySelector('#quantity')
-const addToCartButton = document.querySelector('#addToCart')
+const image = document.querySelector(".item__img");
+const title = document.querySelector("#title");
+const price = document.querySelector("#price");
+const description = document.querySelector("#description");
+const colorSelectElement = document.querySelector("#colors");
+const quantitySelectElement = document.querySelector("#quantity");
+const addToCartButton = document.querySelector("#addToCart");
 
 // Récupération de l'id sur le produit séléctionné en page d'accueil via params
-const params = new URL(document.location).searchParams
-const id = params.get('id')
-
-// Lecture des clés et objets stockées dans le local storage
-let produitsEnregistresDansLocalStorage = JSON.parse(localStorage.getItem('panier'));
-
-/*------------------------*/
+const params = new URL(document.location).searchParams;
+const id = params.get("id");
 
 // Création des fonctions
 
-// Affichage du produit séléctionné en amont sur la page d'accueil
-function displayProduct(product) {
-  image.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`
-    title.innerHTML = `<h1>${product.name}</h1>`
-    price.innerHTML = `<p>${product.price}</p>`
-    description.innerHTML = `<p>${product.description}</p>`
-    product.colors.forEach((color) => colorSelectElement.innerHTML += `<option value="${color}">${color}</option>`)
+/**
+ * @param key {string}
+ * @returns {any}
+ */
+function getFromLocalStorage(key) {
+  return JSON.parse(localStorage.getItem(key));
 }
 
-// Fonction PopupConfirmation demandant a l'utilisateur si il souhaite aller au panier ou continuer ses achats
-const popupConfirmation = () => {
-  if (window.confirm(`le produit: ${id} avec la couleur: ${colorSelectElement.value} a bien été ajouté au panier. 
-  aller au panier OK ou revenir a l'accueil ANNULER`)) {
-    window.location.href = 'cart.html'
+/**
+ * @param key {string}
+ * @param item {any}
+ * @returns {any}
+ */
+function saveToLocalStorage(key, item) {
+  localStorage.setItem(key, JSON.stringify(item));
+}
+
+// Affichage du produit séléctionné en amont sur la page d'accueil
+function displayProduct(product) {
+  image.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
+  title.innerHTML = `<h1>${product.name}</h1>`;
+  price.innerHTML = `<p>${product.price}</p>`;
+  description.innerHTML = `<p>${product.description}</p>`;
+  product.colors.forEach(
+    (color) =>
+      (colorSelectElement.innerHTML += `<option value="${color}">${color}</option>`),
+  );
+}
+
+// Affichage d'un message d'erreur en cas de mauvaise séléction
+function checkProductSelection() {
+  if (colorSelectElement.value === "" || quantitySelectElement.value <= 0) {
+    alert("Vous devez séléctionner une couleur ET une quantité");
+    return false;
+  }
+  return true;
+}
+
+// Fonction pour Sauvegarder le produit dans le local storage et mettre à jour la quantité
+function saveProduct() {
+  // Récupération des données du local storage insérées dans variable 'cart'
+  let cart = getFromLocalStorage("cart");
+
+  // Si la clé 'cart' n'éxiste pas dans le local storage, on créé un tableau 'cart' vide
+  if (!cart) {
+    cart = [];
+  }
+
+  // Si le même produit/couleur est déjà dans le panier, on mets à jour la quantité
+  let productAlreadyInCart = cart.find((item) => item.id === id && item.color === colorSelectElement.value,);
+  if ("cart" in localStorage && productAlreadyInCart) {
+    if (productAlreadyInCart != undefined) {
+      console.log("Article déjà au panier");
+      let newQuantity = Number(quantitySelectElement.value) + Number(productAlreadyInCart.quantity);
+      productAlreadyInCart.quantity = newQuantity.toString();
+    }
   } else {
-    window.location.href = 'index.html'
+    cart.push({
+      id,
+      color: colorSelectElement.value,
+      quantity: quantitySelectElement.value,
+    });
+  }
+
+  saveToLocalStorage("cart", cart);
+}
+
+// Au clic sur le bouton 'ajouter au panier', éxecuter la fonction 'saveProduct'
+function onClickAddToCart() {
+  if (checkProductSelection()) {
+    saveProduct();
   }
 }
 
-// Création de la fonction addToCart permettant de gérer l'envoi des données vers le panier
-
-function addToCart() {
-
-  // Si aucun produit n'est séléctionné, alors un message d'erreur apparait au clic sur le bouton "ajouter au panier"
-  addToCartButton.addEventListener('click', function () {
-    if (colorSelectElement.value === "" || quantitySelectElement.value <= 0) {
-      alert('Vous devez séléctionner une couleur ET une quantité')
-      return;
-    }
-
-    function addToLocalStorage () {
-      produitsEnregistresDansLocalStorage.push(cartInit);
-      localStorage.setItem('panier', JSON.stringify(produitsEnregistresDansLocalStorage))
-      popupConfirmation();
-    }
-
-    // Objet qui va acceuillir les données séléctionnées via les input
-    let cartInit = {
-      id_produit: id,
-      couleur: colorSelectElement.value,
-      quantité: quantitySelectElement.value
-    }
-
-    // Si il y a déjà une clé 'panier' dans le local storage, ajouter les données séléctionnés
-    if (produitsEnregistresDansLocalStorage) {
-      addToLocalStorage()
-    }
-    // Si il n'y a pas de clé 'panier' dans le local storage, création de la clé panier et du tableau
-    else {
-      produitsEnregistresDansLocalStorage = []
-      addToLocalStorage()
-    }
-  })
-}
-addToCart()
+addToCartButton.addEventListener("click", onClickAddToCart);
 
 // Récupération des données de l'API
-    fetch(`http://localhost:3000/api/products/${id}`)
-    .then(response => response.json())
-    .then(product => displayProduct(product))
-    .catch(err => console.error(err))
+ fetch(`http://localhost:3000/api/products/${id}`)
+  .then((response) => response.json())
+  .then((product) => displayProduct(product))
+  .catch((err) => console.error(err))
